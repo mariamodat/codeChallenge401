bd599e1b1f18a77d997ebd9565efaadc8d211fb2
mariamodat <mariamodat0@gmail.com>
2021-07-11 15:12:22 +0300

    done Peusodo Queue

--- stacksAndQueues/app/src/main/java/stacksAndQueues/QNode.java
+++ stacksAndQueues/app/src/main/java/stacksAndQueues/QNode.java
@@ -1,2 +1,24 @@
-package stacksAndQueues;public class QNode {
+package stacksAndQueues;
+
+public class QNode {
+int data;
+QNode next ;
+
+  public QNode(int data) {
+    this.data = data;
+    this.next=null ;
+  }
+
+  public int getData() {
+    return data;
+  }
+
+
+  public QNode getNext() {
+    return next;
+  }
+
+  public void setNext(QNode next) {
+    this.next = next;
+  }
 }
